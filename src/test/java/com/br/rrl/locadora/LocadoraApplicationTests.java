5083effb85a9a388462439000f2f0e97e42347b5
Raphael Ribeiro Lino <raphar.lino@gmail.com>
2023-06-15 15:36:34 -0300

    Criando projeto

---
+++ src/test/java/com/br/rrl/locadora/LocadoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.rrl.locadora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocadoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
