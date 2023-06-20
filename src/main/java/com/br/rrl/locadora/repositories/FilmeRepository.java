dc0c26aeabb285ea5b9312e7f89298b1d5de17af
Raphael Ribeiro Lino <raphar.lino@gmail.com>
2023-06-20 20:14:37 -0300

    Adicionando filme repository

---
+++ src/main/java/com/br/rrl/locadora/repositories/FilmeRepository.java
@@ -0,0 +1,9 @@
+package com.br.rrl.locadora.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.br.rrl.locadora.entities.FilmeEntity;
+
+public interface FilmeRepository extends JpaRepository<FilmeEntity, Long>{
+
+}
